7277b2363adf081b537c6098e98f721db246431e
hqx871 <hqx871@gmail.com>
2021-07-12 10:37:33 +0800

    cstore

--- presto-cstore/src/main/java/com/facebook/presto/cstore/storage/MemoryStorageManager.java
+++ presto-cstore/src/main/java/com/facebook/presto/cstore/storage/MemoryStorageManager.java
@@ -34,7 +34,6 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.nio.file.Paths;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
